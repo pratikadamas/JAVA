3adebe3ee207403085ffcb4e80faf7571d90b757
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-01-08 01:31:58 +0530

    Bitwise operator in java

---
+++ Bitwiseopr.java
@@ -0,0 +1,21 @@
+public class Bitwiseopr {
+    public static void main(String[] args)
+    {
+    int a = 2, b = 3;
+    int c=a<<b;
+    int d=a>>b;
+    int e=~a;
+    int s=-1>>>25;
+    System.err.println("value of left shift "+c);
+    System.err.println("value of  signed right shift "+d);
+    System.err.println("value of nagation "+e);
+    System.err.println("value of  unsigned right shift "+s);
+
+
+
+
+
+
+
+    }
+}
