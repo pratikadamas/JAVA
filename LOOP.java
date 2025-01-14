02bc6d2e5c569dfa5cb10af829a4e84d95cc192a
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-01-14 19:52:55 +0530

    Sum of n natural number using while loop

---
+++ LOOP.java
@@ -0,0 +1,28 @@
+
+ import java.util.Scanner;
+
+public class LOOP
+ {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int x =sc.nextInt();
+        int i =1,sum =0;
+         
+        while(i<=x){
+            sum+=i;
+            i++;
+        }
+
+        System.out.println("Sum of N natural number is "+sum);
+
+
+
+        
+
+    }
+
+    
+}
