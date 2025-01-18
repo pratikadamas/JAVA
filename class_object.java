92016164648e695e637253e80fb4322c14384fbf
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-01-18 23:51:58 +0530

    class & object in java

---
+++ class_object.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+class student{
+    int roll;
+    String name;
+    void setdata(int roll,String name)
+    {
+        this.roll=roll;
+        this.name=name;
+    }
+    void show()
+    {
+        System.out.println("Student name: "+name);
+        System.out.println("Student roll no: "+roll);
+    }
+
+}
+
+public class class_object {
+    public static void main(String []args){
+     student s1=new student();
+     s1.setdata(126, "Pratik Giri");
+     s1.show();
+    }
+}
