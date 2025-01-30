9fc62bbaf3ff953a4554cb846b7d2f1be50d1469
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-01-30 19:58:21 +0530

    if-else statement in java

---
+++ if_else.java
@@ -0,0 +1,21 @@
+
+
+
+// condition always boolean type
+
+class if_else {
+    public static void main(String[ ] args )
+    {
+
+   boolean b =true;
+ if(b)
+ {
+    System.out.println("true");
+ }
+ else{
+    System.out.print("False");
+ }
+
+    }
+    
+}
