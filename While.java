dd72429a232437349baf56847c30429ae66daab6
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-01-31 23:56:04 +0530

    Switch case in java

--- While.java
+++ While.java
@@ -2,7 +2,7 @@ public class While {
     public static void main(String[] args) {
         int i = 0;
         while (true) {
-            System.out.println(i);
+            System.out.println(++i);
             
         }
         System.out.println("End of the program");
