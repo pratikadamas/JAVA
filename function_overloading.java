4798f9e36acc369092d3db3d937a30002a60eeff
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-02-12 02:03:01 +0530

    Method overloading in java

--- function_overloading.java
+++ function_overloading.java
@@ -7,6 +7,12 @@ class overload{
     public void m1(int... f){
         System.out.println("var_args");
     }
+    public void m1(String s){
+        System.out.println("string args");
+    }
+    public void m1(StringBuffer sb){
+        System.out.println("stringbuffer args");
+    }
 }
 public class function_overloading 
 {
@@ -16,7 +22,9 @@ public class function_overloading
         obj.m1();
         obj.m1(10);
         obj.m1(10,20);
-        obj.m1(10,20,30);
+        // obj.m1(10,20,30);
+        // obj.m1("hello");
+        // obj.m1(new StringBuffer("hello"));
     }
     
 }
