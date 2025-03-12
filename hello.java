e7060c3f0e737146975b8ec1eadd764c26aeb742
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-03-13 01:47:10 +0530

    two main function in java

---
+++ hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class hii{
+    public static void main(String args[]){
+        System.out.println("hello 2");
+    }
+}
+
+
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello   1");
+    }
+}
