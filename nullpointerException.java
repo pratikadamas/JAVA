73127b396a6290b5e1749ded29d2dee58749ab44
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-03-30 19:39:46 +0530

    Java Program to illustrate the use of Null Pointer Exception in Java

---
+++ nullpointerException.java
@@ -0,0 +1,14 @@
+//Java Program to illustrate the use of Null Pointer Exception in Java  
+public class nullpointerException {
+    public static void main(String[] args) {
+        String str = null; // Initializing a String variable to null    
+        try {
+            int length = str.length(); // Attempting to call a method on a null reference    
+            System.out.println("Length of the string: " + length);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+            System.out.println("Error: Null reference encountered.");
+            // Additional error handling code can be added here    
+        }
+    }
+} 
