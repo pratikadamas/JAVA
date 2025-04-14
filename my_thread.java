71f6af09323eaabd4d7f92da08afdbc27fd89b06
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-04-14 12:36:24 +0530

    thread implements Runnable interfaces

--- my_thread.java
+++ my_thread.java
@@ -1,25 +1,34 @@
-public class my_thread extends Thread
-{
-    public static void main(String[] args) throws InterruptedException
-    {
-        my_thread t1 = new my_thread();
-        System.out.println(t1.getState());
-        t1.start();
-        System.out.println(t1.getState());
-        System.out.println(Thread.currentThread().getState());
-        Thread.sleep(1000);
-        System.out.println(t1.getState());
+class my_thread implements Runnable{
 
+    my_thread(String name){
+        Thread t = new Thread(this,name);
+        t.start();
+        System.out.println(t.getName());
 
     }
-    public void run()
-        {
-        try {
-            System.out.println("RUNNING");
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    public void run() {
+//        System.out.println(t.getName());
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            try{
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                System.out.println(e);
+            }
         }
+        System.out.println(Thread.currentThread().getName()+" ended!");
+    }
 
+
+
+    public static void main(String[] args) {
+        System.out.println("main started!!");
+        my_thread obj1=new my_thread("obj1");
+        my_thread obj2=new my_thread("obj2");
+        System.out.println("main ended!!");
+//
     }
 }
+
+
