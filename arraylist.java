7b8166f3738b40afa3415a5ff88857d43c977ce6
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-04-17 01:34:00 +0530

    ArrayList in java

--- arraylist.java
+++ arraylist.java
@@ -1,5 +1,7 @@
-import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+import java.util.*;
 public class arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> mark=new ArrayList<Integer>();
@@ -15,6 +17,17 @@ public class arraylist {
         System.out.println("after sorting marks a");
         Collections.sort(mark);
         for(int x:mark){
+            System.out.println(" "+x);
+        }
+        System.out.println("in reverse order marks ");
+        Collections.reverse(mark);
+        for(int x:mark){
+            System.out.println(" "+x);
+        }
+
+       int  arr[]={2,5,6,1,0};
+        Arrays.sort(arr);
+        for(int x:arr){
             System.out.println(" "+x);
         }
     }
